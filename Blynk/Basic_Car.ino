e7dab5c5b7d63951737a6e991cd521b736b11809
Chokdee-Sigazen <choikdee50@gmail.com>
2024-05-10 01:10:22 +0700

    add Blynk Car git push

---
+++ Blynk/Basic_Car.ino
@@ -0,0 +1,106 @@
+/*************************************************************
+
+  This is a simple demo of sending and receiving some data.
+  Be sure to check out other examples!
+ *************************************************************/
+
+/* Fill-in information from Blynk Device Info here */
+#define BLYNK_TEMPLATE_ID "TEMPLATEID"
+#define BLYNK_TEMPLATE_NAME "TEMPLATENAME"
+#define BLYNK_AUTH_TOKEN "AUTHTOKEN"
+
+/* Comment this out to disable prints and save space */
+#define BLYNK_PRINT Serial
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+
+int motor1Pin1 = 19;
+int motor1Pin2 = 18;
+int motor1Pin3 = 23;
+int motor1Pin4 = 22;
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "SSID";
+char pass[] = "PASSWORD";
+
+BlynkTimer timer;
+
+// This function is called every time the Virtual Pin 0 state changes
+BLYNK_WRITE(V0)
+{
+    // Set incoming value from pin V0 to a variable
+    int value = param.asInt();
+    if (value == 0)
+    {
+        digitalWrite(motor1Pin1, LOW);
+        digitalWrite(motor1Pin2, LOW);
+    }
+    if (value == 1)
+    {
+        digitalWrite(motor1Pin1, HIGH);
+        digitalWrite(motor1Pin2, LOW);
+    }
+    if (value == -1)
+    {
+        digitalWrite(motor1Pin1, LOW);
+        digitalWrite(motor1Pin2, HIGH);
+    }
+    Serial.println("LEFT:" + String(value));
+}
+
+BLYNK_WRITE(V1)
+{
+    // Set incoming value from pin V0 to a variable
+    int value = param.asInt();
+    if (value == 0)
+    {
+        digitalWrite(motor1Pin3, LOW);
+        digitalWrite(motor1Pin4, LOW);
+    }
+    if (value == 1)
+    {
+        digitalWrite(motor1Pin3, HIGH);
+        digitalWrite(motor1Pin4, LOW);
+    }
+    if (value == -1)
+    {
+        digitalWrite(motor1Pin3, LOW);
+        digitalWrite(motor1Pin4, HIGH);
+    }
+    Serial.println("RIGHT:" + String(value));
+}
+
+// This function is called every time the device is connected to the Blynk.Cloud
+BLYNK_CONNECTED()
+{
+    Blynk.setProperty(V3, "offImageUrl", "https://static-image.nyc3.cdn.digitaloceanspaces.com/general/fte/congratulations.png");
+    Blynk.setProperty(V3, "onImageUrl", "https://static-image.nyc3.cdn.digitaloceanspaces.com/general/fte/congratulations_pressed.png");
+    Blynk.setProperty(V3, "url", "https://docs.blynk.io/en/getting-started/what-do-i-need-to-blynk/how-quickstart-device-was-made");
+}
+
+void myTimerEvent()
+{
+    Blynk.virtualWrite(V2, millis() / 1000);
+}
+
+void setup()
+{
+    // Debug console
+    pinMode(motor1Pin1, OUTPUT);
+    pinMode(motor1Pin2, OUTPUT);
+    pinMode(motor1Pin3, OUTPUT);
+    pinMode(motor1Pin4, OUTPUT);
+    Serial.begin(115200);
+
+    Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+    timer.setInterval(1000L, myTimerEvent);
+}
+
+void loop()
+{
+    Blynk.run();
+    timer.run();
+}
