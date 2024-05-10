2dd94574ab4a0e5faadefcda4251e48497f16fc2
TripleKdev <80637250+kangkengkhadev@users.noreply.github.com>
2024-05-11 06:30:01 +0700

    Update L298N_Basic.ino

--- L298N/L298N_Basic.ino
+++ L298N/L298N_Basic.ino
@@ -1,14 +1,14 @@
 
 int motor1Pin1 = 23; 
 int motor1Pin2 = 22; 
-int motor1Pin3 = 19; 
-int motor1Pin4 = 18; 
+int motor2Pin1 = 19; 
+int motor2Pin2 = 18; 
 void setup() {
   // sets the pins as outputs:
   pinMode(motor1Pin1, OUTPUT);
   pinMode(motor1Pin2, OUTPUT);
-  pinMode(motor1Pin3, OUTPUT);
-  pinMode(motor1Pin4, OUTPUT);
+  pinMode(motor2Pin1, OUTPUT);
+  pinMode(motor2Pin2, OUTPUT);
   // attach the channel to the GPIO to be controlled
 
   Serial.begin(115200);
@@ -22,31 +22,31 @@ void loop() {
   Serial.println("Moving Forward");
   digitalWrite(motor1Pin1, HIGH);
   digitalWrite(motor1Pin2, LOW);
-   digitalWrite(motor1Pin3, HIGH);
-  digitalWrite(motor1Pin4, LOW);  
+   digitalWrite(motor2Pin1, HIGH);
+  digitalWrite(motor2Pin2, LOW);  
   delay(2000);
 
   // Stop the DC motor
   Serial.println("Motor stopped");
   digitalWrite(motor1Pin1, LOW);
   digitalWrite(motor1Pin2, LOW);
-  digitalWrite(motor1Pin3, LOW);
-  digitalWrite(motor1Pin4, LOW);
+  digitalWrite(motor2Pin1, LOW);
+  digitalWrite(motor2Pin2, LOW);
   delay(1000);
 
   // Move DC motor backwards at maximum speed
   Serial.println("Moving Backwards");
   digitalWrite(motor1Pin1, LOW);
   digitalWrite(motor1Pin2, HIGH);
-   digitalWrite(motor1Pin3, LOW);
-  digitalWrite(motor1Pin4, HIGH);  
+   digitalWrite(motor2Pin1, LOW);
+  digitalWrite(motor2Pin2, HIGH);  
   delay(2000);
 
   // Stop the DC motor
   Serial.println("Motor stopped");
   digitalWrite(motor1Pin1, LOW);
   digitalWrite(motor1Pin2, LOW);
-  digitalWrite(motor1Pin3, LOW);
-  digitalWrite(motor1Pin4, LOW);
+  digitalWrite(motor2Pin1, LOW);
+  digitalWrite(motor2Pin2, LOW);
   delay(1000);
 }
