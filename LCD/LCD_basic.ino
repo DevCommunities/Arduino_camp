033f100407d73562be12b773a91042ec7d866dcb
Chokdee Prushyapong <68228264+Chokdee-Sigazen@users.noreply.github.com>
2024-05-11 08:17:37 +0700

    Update LCD_basic.ino

--- LCD/LCD_basic.ino
+++ LCD/LCD_basic.ino
@@ -1,24 +1,20 @@
-#include <LiquidCrystal.h> 
-LiquidCrystal lcd(1, 2, 4, 5, 6, 7); 
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 16, 2);   //Module IIC/I2C Interface บางรุ่นอาจจะใช้ 0x3f
 
-void setup() { 
-  lcd.begin(16,2);
+void setup()
+{
+lcd.begin();
+//lcd.noBacklight();   // ปิด backlight
+lcd.backlight();       // เปิด backlight 
+
+  lcd.home();
+  lcd.print("Hello, world!");
+  lcd.setCursor(0, 1);
+  lcd.print("www.9Arduino.com");
 }
 
-void loop() { 
-  lcd.print("Arduino");
-  delay(3000); 
-  lcd.setCursor(2,1); 
-  lcd.print("LCD Tutorial"); 
-  delay(3000); 
-  lcd.clear(); 
-  lcd.blink(); 
-  delay(4000); 
-  lcd.setCursor(7,1); 
-  delay(3000); 
-  lcd.noBlink(); 
-  lcd.cursor(); 
-  delay(4000); 
-  lcd.noCursor(); 
-  lcd.clear(); 
+void loop() {
+
+ 
+
 }
